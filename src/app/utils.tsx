d2a2f8f6facb2b449908ce32886cb4238bae8648
Piotr Milewski <milewski.piotr@gmail.com>
2023-09-07 01:14:57 +0200

    Product list (#2)
    * Product List
    
    * Add types
    
    * dff

---
+++ src/app/utils.tsx
@@ -0,0 +1,34 @@
+export const products = [
+	{
+		name: "Intelligent Bronze Towels",
+		slug: "Intelligent-Bronze-Towels",
+		description:
+			"The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+		price: "491.00",
+		imageUrl: "http://placekitten.com/300/300?image=1",
+	},
+	{
+		name: "Elegant Rubber Keyboard",
+		slug: "Elegant-Rubber-Keyboard",
+		description:
+			"The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+		price: "132.00",
+		imageUrl: "http://placekitten.com/300/300?image=2",
+	},
+	{
+		name: "Small Granite Bacon",
+		slug: "Small-Granite-Bacon",
+		description:
+			"The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+		price: "345.00",
+		imageUrl: "http://placekitten.com/300/300?image=3",
+	},
+	{
+		name: "Rustic Plastic Table",
+		slug: "Rustic-Plastic-Table",
+		description:
+			"The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients",
+		price: "204.00",
+		imageUrl: "http://placekitten.com/300/300?image=4",
+	},
+];
